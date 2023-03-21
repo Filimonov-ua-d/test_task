1686e9507363f24c984a7aacd55b594961fc0ef8
Filimonov-ua-d <filimonov.d.2001@icloud.com>
2023-03-21 20:23:39 +0100

    add server init

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/Filimonov-ua-d/test_task/cmd/config"
+	"github.com/Filimonov-ua-d/test_task/server"
+	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	var db *sqlx.DB
+
+	if err := config.Init(); err != nil {
+		log.Fatalf("%s", err.Error())
+	}
+
+	app := server.NewApp(db)
+
+	if err := app.Run(viper.GetString("port")); err != nil {
+		log.Fatalf("%s", err.Error())
+	}
 }
--- server/app.go
+++ server/app.go
@@ -1,1 +1,68 @@
 package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
+	"github.com/spf13/viper"
+)
+
+type App struct {
+	httpServer *http.Server
+}
+
+func NewApp(db *sqlx.DB) *App {
+
+	var err error
+
+	user := viper.GetString("postgres.user")
+	password := viper.GetString("postgres.password")
+	dbname := viper.GetString("postgres.dbname")
+	sslmode := viper.GetString("postgres.sslmode")
+	host := viper.GetString("postgres.host")
+	port := viper.GetString("postgres.port")
+
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=%s host=%s port=%s",
+		user, password, dbname, sslmode, host, port)
+
+	if db, err = sqlx.Connect("postgres", dsn); err != nil {
+		log.Panic(err)
+	}
+
+	return &App{}
+}
+
+func (a *App) Run(port string) error {
+	router := gin.Default()
+
+	a.httpServer = &http.Server{
+		Addr:           ":" + port,
+		Handler:        router,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go func() {
+		if err := a.httpServer.ListenAndServe(); err != nil {
+			log.Fatalf("running server error: %s", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, os.Interrupt)
+
+	<-quit
+
+	ctx, shutdown := context.WithTimeout(context.Background(), 5*time.Second)
+	defer shutdown()
+
+	return a.httpServer.Shutdown(ctx)
+}
